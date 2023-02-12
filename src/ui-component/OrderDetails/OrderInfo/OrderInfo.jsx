083eea1dd182b2804682abf4d65d18a8958f582c
Jayden Nguy <tai.nguy22112000@gmail.com>
2023-02-12 20:04:13 +0700

    feat: add order detail, payment, delivery

---
+++ src/ui-component/OrderDetails/OrderInfo/OrderInfo.jsx
@@ -0,0 +1,12 @@
+import { Paper } from '@mui/material';
+import * as React from 'react';
+
+const OrderInfo = (props) => {
+  return (
+    <Paper elevation={2} sx={{ padding: 2 }}>
+      Chi tiết đơn hàng
+    </Paper>
+  );
+};
+
+export default OrderInfo;
---
+++ src/ui-component/OrderDetails/OrderPayment/OrderPayment.jsx
@@ -0,0 +1,12 @@
+import { Paper } from '@mui/material';
+import * as React from 'react';
+
+const OrderPayment = (props) => {
+  return (
+    <Paper elevation={2} sx={{ padding: 2 }}>
+      Thanh toán
+    </Paper>
+  );
+};
+
+export default OrderPayment;
